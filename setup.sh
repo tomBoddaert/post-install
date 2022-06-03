898436862c5340cdb8b6a76f1614d5c66b0ec2b3
tomBoddaert <70723965+tomBoddaert@users.noreply.github.com>
2022-06-03 10:07:04 +0100

    sshd_config fix
    Before, the contents of "sshd_config" would be blank

--- setup.sh
+++ setup.sh
@@ -63,7 +63,7 @@ git clone https://github.com/nvm-sh/nvm.git $NVM_DIR -q
 echo -e "\nSetting up ssh server (port 2222)..."
 [[ -f ./authorized_keys ]] && (echo "Found 'authorized_keys' file" && mkdir ${SUDO_USER_HOME}/.ssh 2>/dev/null && chmod 700 ${SUDO_USER_HOME}/.ssh && touch ${SUDO_USER_HOME}/.ssh/authorized_keys && chmod 600 ${SUDO_USER_HOME}/.ssh/authorized_keys && cat ./authorized_keys >> ${SUDO_USER_HOME}/.ssh/authorized_keys && chown -R $SUDO_USER ${SUDO_USER_HOME}/.ssh)
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.default
-sed 's/^#Port 22$/Port 2222/; s/^#PasswordAuthentication yes$/PasswordAuthentication no/' /etc/ssh/sshd_config > /etc/ssh/sshd_config
+sed 's/^#Port 22$/Port 2222/; s/^#PasswordAuthentication yes$/PasswordAuthentication no/' /etc/ssh/sshd_config.default > /etc/ssh/sshd_config
 ufw allow 2222 1>/dev/null
 systemctl restart ssh
 
